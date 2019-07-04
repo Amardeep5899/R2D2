8d8b0b84645b4e52b45a379391d3f82ecdaff510
Amardeep5899 <38188381+Amardeep5899@users.noreply.github.com>
2019-07-04 16:51:13 +0530

    Add files via upload

---
+++ R2D2 (2).py
@@ -0,0 +1,23 @@
+print("Enter the index number to display movie information")
+print("1. Iron Man")
+print("2. Avengers")
+print("3. Men in Black")
+print("4. Spider Man")
+n=int(input())
+if(n==1):
+    print("movie name: Iron Man")
+    print("Rating: 7.5/10")
+    print("Year of release: 2013")
+elif(n==2):
+    print("movie name: Avengers")
+    print("Rating: 8.5/10")
+    print("Year of release: 2015")
+elif(n==3):
+    print("movie name: Men in Black")
+    print("Rating: 5.5/10")
+    print("Year of release: 2019")
+elif(n==4):
+    print("movie name: Spider Man")
+    print("Rating: 6.5/10")
+    print("Year of release: 2019")
+
